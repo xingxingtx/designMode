a8216ab903c17e5a1bd6fa99f494fc177032443b
wei.peng <wei.peng@aorise.org>
2018-08-16 09:21:38 +0800

    组合模式

---
+++ src/design/model/Component/ImageFile.java
@@ -0,0 +1,13 @@
+package design.model.Component;
+
+/**
+ * Created by Administrator on 2018/8/16.
+ * 文件（灰色节点）和文件夹（白色节点）两类不同的元素，其中在文件夹中可以包含文件，还可以继续包含子文件夹，
+ * 但是在文件中不能再包含子文件或者子文件夹。在此，我们可以称文件夹为容器(Container)，而不同类型的各种文件是其成员，
+ * 也称为叶子(Leaf)，一个文件夹也可以作为另一个更大的文件夹的成员。
+ */
+public class ImageFile {
+
+
+}
+
