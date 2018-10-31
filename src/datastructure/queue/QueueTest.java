a5b409c5a67f7e5126e11744f8e41fd8aa330690
wei.peng <982084398@qq.com>
2018-10-31 13:48:12 +0800

    红黑树

---
+++ src/datastructure/queue/QueueTest.java
@@ -0,0 +1,13 @@
+package datastructure.queue;
+
+
+/**
+ * Created by Administrator on 2018/10/25.
+ */
+public class QueueTest {
+
+    public static void main(String[] args) {
+        boolean str = "rgerg t3tfhw4hrhhj".contains("w4hrq");
+        System.out.println(str);
+    }
+}
